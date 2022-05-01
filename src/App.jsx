6712baa61e6db0fdd4c2dcbc8cab983928f503eb
maniacinahuel <nahuelmaniaci@gmail.com>
2022-05-01 20:19:39 -0300

    NavBar

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+import React from 'react';
+import NavBar from './components/NavBar.jsx';
+
+import './style.css';
+
+export default function App() {
+  return (
+    <>
+      <NavBar />
+    </>
+  );
+}
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+
+//import mi archivo css
+import './styles/NavBar.css';
+
+const NaBar = () => {
+  return (
+    <header className="header">
+      <div className="logo-container">
+        <img src="media/swimming_logo.png" alt="logo" />
+      </div>
+      <h1>Swimming in Circles</h1>
+      <nav>
+        <ul className="nav-container">
+          <li>
+            <a href="/">Inicio</a>
+          </li>
+          <li>
+            <a href="/">Productos</a>
+          </li>
+          <li>
+            <a href="/">Blog</a>
+          </li>
+          <li>
+            <a href="/">Contacto</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default NaBar;
